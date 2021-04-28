b9e426ae8aa80b4356405072c7694fea3b7d7f58
anishagarodia <46638636+anishagarodia@users.noreply.github.com>
2021-04-28 12:18:31 -0500

    added java docs

--- src/Main/Grapher.java
+++ src/Main/Grapher.java
@@ -23,6 +23,13 @@ import java.util.Vector;
 
 public class Grapher {
 
+	/**
+	 * makes a stock price graph over a specific given time period
+	 * @param vals stock price values
+	 * @param dates range of dates
+	 * @param name stock ticker
+	 */
+	
 	public static void makeGraph(Vector<Double> vals, Vector<Long> dates, String name) {
 		XYSeries series = new XYSeries("Price");
 		addDates(series, vals, dates);
@@ -39,6 +46,12 @@ public class Grapher {
 		
 	}
 	
+	/**
+	 * adds dates and values to the series
+	 * @param series of dates and values
+	 * @param vals stock price values
+	 * @param dates range of dates
+	 */
 	public static void addDates(XYSeries series, Vector<Double> vals, Vector<Long> dates) {
 		for (int i =0; i<vals.size(); i++) {
 			try {
@@ -50,6 +63,10 @@ public class Grapher {
 		}
 	}
 	
+	/**
+	 * prompts the user and saves graph as a png file
+	 * @param plot the stock price graph
+	 */
 	public static void saveChart(XYPlot plot) {
 		System.out.println("Enter the name of the file you would like to save to");
 		String file = UI.takeUserInput();
@@ -61,6 +78,11 @@ public class Grapher {
 		}
 	}
 	
+	/**
+	 * sets the ranges for the graph
+	 * @param plot the stock price graph
+	 * @param series the series of dates and values
+	 */
 	public static void setRange(XYPlot plot, XYSeries series) {
 		NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
 		double min = series.getMinY()-10;
@@ -71,14 +93,33 @@ public class Grapher {
 		plot.setRangeAxis(0,rangeAxis);
 	}
 	
-	static void parseData(JSONObject j, String name) {
+	/**
+	 * calls the API and makes calls to extract data and make the graph
+	 * @param j  the JSON Object for the stock
+	 * @param name the stock ticker name
+	 */
+	public static void parseData(JSONObject j, String name) {
 		JSONArray vals1 = (JSONArray) j.get("c");
 		JSONArray dates1 = (JSONArray) j.get("t");
 		Vector<Double> vals = new Vector<Double>();
 		Vector<Long> dates = new Vector<Long>();
 		
-		for (int i = 0; i < vals1.size(); i++){
-			
+		extractValues(vals1, dates1, vals, dates);
+		
+		makeGraph(vals,dates, name);
+		
+	}
+	
+	
+	/**
+	 * extracts stock price values over a specific date range
+	 * @param vals1 JSON Array of stock values
+	 * @param dates1 JSON Array of dates
+	 * @param vals Vector to hold stock values
+	 * @param dates Vector to hold dates
+	 */
+	public static void extractValues(JSONArray vals1, JSONArray dates1, Vector<Double> vals, Vector<Long> dates) {
+		for (int i = 0; i < vals1.size(); i++){	
 			try {
 			double val = ((double) vals1.get(i));
 			}
@@ -91,11 +132,8 @@ public class Grapher {
 			vals.add(val);
 			dates.add(d);
 			
-			
-			
 		}
-		makeGraph(vals,dates, name);
-		
 	}
+	
 	
 }
